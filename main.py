dd9e84a05076dbff7a8b6c519fcf7f903a67a798
MelnikovAP <you@example.com>
2022-06-17 16:21:37 +0200

    added creating of data folder

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 from silx.gui import qt
 from mainWindow import mainWindow
+import os
 
 
 if __name__ == "__main__":
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
     app = qt.QApplication(sys.argv)
     example = mainWindow()
     example.show()
