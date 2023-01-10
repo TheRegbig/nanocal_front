fece0be64274d460de2bcac5bbf58aa2c954e59e
MelnikovAP <you@example.com>
2023-01-10 13:44:57 +0100

    backup 10/01/23

--- mainWindow.py
+++ mainWindow.py
@@ -285,3 +285,6 @@ class Dict2Class(object):
     def __init__(self, my_dict):
         for key in my_dict:
             setattr(self, key, my_dict[key])
+        self.my_dict = my_dict
+    def get_dict(self):
+        return self.my_dict
